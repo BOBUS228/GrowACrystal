a88d9d4a83858376a8a0b02ebab16c4e675769ae
Dan7zpcST3modmaker <155840819+Dan7zpcST3modmaker@users.noreply.github.com>
2026-06-17 15:15:25 +0400

    maybe model will work now

--- src/main/java/com/cakeov/grow_a_crystal/block/custom/Crystal.java
+++ src/main/java/com/cakeov/grow_a_crystal/block/custom/Crystal.java
@@ -1,9 +1,16 @@
 package com.cakeov.grow_a_crystal.block.custom;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class Crystal extends Block {
     public Crystal(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 }
